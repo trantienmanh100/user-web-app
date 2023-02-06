6ea0ddd11288aac1e12bcb11dd1e3d0963498c6f
DucToan <toanddph17196@fpt.edu.vn>
2023-02-06 23:34:05 +0700

    update/moi

--- src/app/pages/user/qluser/qluser.component.ts
+++ src/app/pages/user/qluser/qluser.component.ts
@@ -4,6 +4,9 @@ import {FormBuilder, Validators} from "@angular/forms";
 import {UserService} from "../../../shared/services/user.service";
 import {ToastrService} from "ngx-toastr";
 import {LocalStorageService} from "ngx-webstorage";
+import CommonUtil from "../../../shared/utils/common-utils";
+import {TranslateService} from "@ngx-translate/core";
+import {NzModalRef, NzModalService} from "ng-zorro-antd/modal";
 
 @Component({
   selector: 'app-qluser',
@@ -18,10 +21,18 @@ export class QLUserComponent implements OnInit {
   userForm = this.fb.group({
     fullName: [this.users.fullName,[Validators.required]],
     userName : this.users.userName,
-    email: [this.users.email,[Validators.required, Validators.email]],
+    email: [this.users.email,[
+      Validators.required,
+      Validators.maxLength(50),
+      Validators.pattern( '^(\\s){0,}[a-zA-Z][a-zA-Z0-9_\\.\-]{1,50}@[a-zA-Z0-9\-_]{2,}(\\.[a-zA-Z0-9\]{2,4}){1,2}(\\s){0,}$',)
+    ]],
     address: [this.users.address, [Validators.required]],
     gender: [this.users.gender],
-    phoneNumber: [this.users.phoneNumber,[Validators.required]],
+    phoneNumber: [this.users.phoneNumber,[
+      Validators.required,
+      Validators.maxLength(12),
+      Validators.pattern( '^(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$',)
+    ]],
     birthday: [this.users.birthday,[Validators.required]],
     password: this.users.password,
     confirmPassword: this.users.confirmPassword,
@@ -32,7 +43,9 @@ export class QLUserComponent implements OnInit {
     private fb: FormBuilder,
     private userService : UserService,
     private toast: ToastrService,
-    private localStorage : LocalStorageService
+    private translateService: TranslateService,
+    private localStorage : LocalStorageService,
+    private modalService: NzModalService
   ) { }
 
   ngOnInit(): void {
@@ -50,15 +63,26 @@ export class QLUserComponent implements OnInit {
   }
 
   updateUser()  {
-    const id = this.userId
-    this.users.confirmPassword = this.users.password
-    this.userService.update(this.users,id).subscribe((res :any)=>{
-      this.users = res.body?.data;
-      this.toast.success('Cập nhật thông tin thành công');
-      console.log(this.users)
-      this.loadData(id)
-    }, (error:any) => {
-      this.toast.error(error.error.message)
-    })
+    const updateUser =CommonUtil.modalConfirm(
+      this.translateService,
+      'Cập nhật thông tin',
+      'Bạn có muốn cập nhập không',
+      {name: 'a'}
+    )
+    const modal: NzModalRef =this.modalService.create(updateUser);
+    modal.afterClose.subscribe((result:{success: boolean; data: any}) =>{
+      if(result?.success){
+        const id = this.userId
+        this.users.confirmPassword = this.users.password
+        this.userService.update(this.users,id).subscribe((res :any)=>{
+          this.users = res.body?.data;
+          this.toast.success('Cập nhật thông tin thành công');
+          console.log(this.users)
+          this.loadData(id)
+        }, (error:any) => {
+          this.toast.error(error.error.message)
+        })
+      }
+      })
   }
 }
